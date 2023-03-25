69c06721f0aff608d410fc46919786b85208631d
Karl Fischer <fishi0x01@gmail.com>
2023-03-25 16:01:43 +0100

    Win 10 22H2

--- win-10-pro-x64/scripts/set-powerplan.ps1
+++ win-10-pro-x64/scripts/set-powerplan.ps1
@@ -1,20 +1,20 @@
 Try {
   Write-Output "Set power plan to high performance"
 
-  $HighPerf = powercfg -l | %{if($_.contains("High performance")) {$_.split()[3]}}
+  $HighPerf = powercfg -l | ForEach-Object { if ($_.contains("High performance")) { $_.split()[3] } }
 
   # $HighPerf cannot be $null, we try activate this power profile with powercfg
-  # 
-  if ($HighPerf -eq $null)
-  {
+  if ($null -eq $HighPerf) {
     throw "Error: HighPerf is null"
   }
 
   $CurrPlan = $(powercfg -getactivescheme).split()[3]
 
-  if ($CurrPlan -ne $HighPerf) {powercfg -setactive $HighPerf}
+  if ($CurrPlan -ne $HighPerf) { powercfg -setactive $HighPerf }
 
-} Catch {
+}
+Catch {
   Write-Warning -Message "Unable to set power plan to high performance"
   Write-Warning $Error[0]
 }
+
